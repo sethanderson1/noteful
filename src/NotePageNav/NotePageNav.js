ebefaae4974ba6701bd33da188abe9319a5b220f
Seth Anderson <sdande02@gmail.com>
2020-04-13 13:33:06 -0700

    start hooks refactor attempt

---
+++ src/NotePageNav/NotePageNav.js
@@ -0,0 +1,47 @@
+import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import CircleButton from '../CircleButton/CircleButton'
+import ApiContext from '../ApiContext'
+import { findNote, findFolder } from '../notes-helpers'
+import './NotePageNav.css'
+
+export default class NotePageNav extends React.Component {
+  static defaultProps = {
+    history: {
+      goBack: () => { }
+    },
+    match: {
+      params: {}
+    }
+  }
+  // *********why have to make shape?
+  static contextType = ApiContext;
+
+  render() {
+    const { notes, folders, } = this.context
+    const { noteId } = this.props.match.params
+    const note = findNote(notes, noteId) || {}
+    const folder = findFolder(folders, note.folderId)
+    console.log('this.props', this.props)
+    return (
+      <div className='NotePageNav'>
+        <CircleButton
+          tag='button'
+          role='link'
+          onClick={() => this.props.history.goBack()}
+          className='NotePageNav__back-button'
+        >
+          <FontAwesomeIcon icon='chevron-left' />
+          <br />
+          Back
+        </CircleButton>
+        {folder && (
+          <h3 className='NotePageNav__folder-name'>
+            {folder.name}
+          </h3>
+        )}
+        {/* *********how does && trick work */}
+      </div>
+    )
+  }
+}
